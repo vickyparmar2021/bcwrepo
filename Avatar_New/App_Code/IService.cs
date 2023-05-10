28b403a77527e8f96bf87f3b9912c81e30cde4ae
aamod joshi <aamod.joshi@hotmail.com>
2023-05-10 13:02:50 +0530

    changes

---
+++ Avatar_New/App_Code/IService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfRestfulService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IHttpService
+    {
+
+        //[OperationContract]
+        //[WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+        //     UriTemplate = "mobileNumberValid?mobile={mobile}")]
+        //Stream mobileNumberValid(string mobile);
+
+        //[OperationContract]
+        //[WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+        //     UriTemplate = "validateOTP?mobile={mobile}&otp={otp}")]
+        //Stream validateOTP(string mobile, string otp);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,RequestFormat=WebMessageFormat.Json,BodyStyle=WebMessageBodyStyle.WrappedRequest,
+             UriTemplate = "/AddAvatarInRoom")]
+        string AddAvatarInRoom(string roomId, string avatarGLB, string username);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest,
+           UriTemplate = "/GetUserList")]
+        string GetUserList(string roomId);
+    }
+}
